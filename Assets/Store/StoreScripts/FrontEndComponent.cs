627819b56f6c89c694730b3c9dcb8cc3d9bfb5d1
Saad Mohammad <52173869+saadmohammad122@users.noreply.github.com>
2021-05-24 19:33:48 -0700

    Added scripts for front end resistor to test

--- Assets/Store/StoreScripts/FrontEndComponent.cs
+++ Assets/Store/StoreScripts/FrontEndComponent.cs
@@ -4,23 +4,73 @@ using UnityEngine;
 
 public class FrontEndComponent : MonoBehaviour
 {
+
+    public Collider input;
+/*
+    public Collider input;
+    public Collider output;
+*/
     // Start is called before the first frame update
     void Start()
     {
-        
+       // print("Hey!");
+       // Debug.Log("hey this works btw!!!");
     }
 
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter(Collision input)
     {
-        foreach(ContactPoint contact in collision.contacts)
+        print("Got to OnCollisionEnter");
+        print(input.gameObject.transform.name);
+        
+        
+        if (input.gameObject.name == "TestCube")
         {
-            print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
-            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            print("Hey!");
+            print(input.gameObject.name);
+            Debug.Log(input.gameObject.name);
         }
+        if (input.gameObject.tag == "BreadBoardHoles")
+        {
+            print("Hey!");
+            print(input.gameObject.name);
+            Debug.Log(input.gameObject.name);
+        }
+
+    }
+    private void OnCollisionStay(Collision col)
+    {
+        print("Got to OnCollisionStay");
+        print(col.gameObject.name);
+
+
+        if (col.gameObject.name == "TestCube")
+        {
+            print("Hey!");
+            print(col.gameObject.name);
+            Debug.Log(col.gameObject.name);
+        }
+        if (col.gameObject.tag == "BreadBoardHoles")
+        {
+            print("Hey!");
+            print(col.gameObject.name);
+            Debug.Log(col.gameObject.name);
+        }
+
     }
 
-    
+
+    /*   private void OnCollisionEnter(Collision collision)
+       {
+           foreach(ContactPoint contact in collision.contacts)
+           {
+               print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+               Debug.DrawRay(contact.point, contact.normal, Color.white);
+           }
+       }
+    */
+
 
 
     // Update is called once per frame
